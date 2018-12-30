dcbc2d7c66882e462614c644fecbfa05fc50e262
Leko <leko.noor@gmail.com>
2018-12-30 19:29:05 +0900

    fix: add missing await

--- packages/core/src/combineParsers.ts
+++ packages/core/src/combineParsers.ts
@@ -1,11 +1,11 @@
 import { Plugin } from "./Plugin";
 
 export function combineParsers(
-  ...plugins: Plugin<any, any>[]
+  ...plugins: { parse: Plugin<any, any>["parse"] }[]
 ): Plugin<any, any>["parse"] {
   return async function parse(body: string, config: any) {
     for (let plugin of plugins) {
-      const parsed = plugin.parse(body, config);
+      const parsed = await plugin.parse(body, config);
       if (parsed) {
         return parsed;
       }
