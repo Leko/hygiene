70515b1ecc216061f9327442b3a3679c6599de70
Leko <leko.noor@gmail.com>
2018-12-30 23:19:15 +0900

    feat: fix order of option groups

--- packages/cli/src/options.ts
+++ packages/cli/src/options.ts
@@ -8,21 +8,25 @@ export const createCommand = ({ plugins }: { plugins: Plugin<any, any>[] }) => {
   const cmd = yargs
     .scriptName("hygiene")
     .option("json", {
+      group: "Global Options",
       describe: "Report as JSON",
       type: "boolean",
       default: false
     })
     .option("ignore-pattern", {
+      group: "Global Options",
       describe: "Pattern of files to ignore",
       type: "string",
       default: null
     })
     .option("ignore-path", {
+      group: "Global Options",
       describe: "Specify path of ignore file",
       type: "string",
       default: findUp(".gitignore")
     })
     .option("ignore", {
+      group: "Global Options",
       describe: "Disable use of ignore files and patterns",
       type: "boolean",
       default: true
@@ -32,14 +36,18 @@ export const createCommand = ({ plugins }: { plugins: Plugin<any, any>[] }) => {
       describe: "",
       // @ts-ignore Type 'Argv<{}>' is missing the following properties from type 'Options': _, json, ignorePath
       handler: run({ plugins }),
-      builder: yargs => yargs.default("glob", "**/*")
+      builder: yargs =>
+        yargs.positional("glob", {
+          describe: "Pattern of target files",
+          default: "**/*"
+        })
     })
     .demandCommand()
     .help();
 
   plugins.forEach(plugin => {
     const options = mapValues(plugin.getConfigDefinition(), option => ({
-      group: plugin.name,
+      group: `Plugin Options: ${plugin.name}`,
       ...option
     }));
     cmd.options(options);
