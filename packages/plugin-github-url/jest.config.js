63e6221437eb8dd41f81359743c7754457143dd1
Leko <leko.noor@gmail.com>
2018-12-30 20:34:04 +0900

    chore: include nested targets

--- packages/plugin-github-url/jest.config.js
+++ packages/plugin-github-url/jest.config.js
@@ -3,5 +3,5 @@ const base = require("../../jest.config.base");
 module.exports = {
   ...base,
   collectCoverageFrom: ["src/**/*.{ts,tsx}"],
-  testMatch: ["**/__tests__/*.+(ts|tsx|js)"]
+  testMatch: ["**/__tests__/**/*.+(ts|tsx|js)"]
 };
